92a2bdb5d250b9f1e697894ec07cc416b558901e
hamza-aissa <professional.aissa@gmail.com>
2025-10-14 09:25:56 +0100

    feat: initial project setup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:waiting_room_app/wiating_room_cart.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Waiting Room',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Waiting Room')),
+        body: const Center(child: WaitingRoomCard(name: 'John Doe')),
+      ),
+    );
+  }
+}
+
---
+++ test/waiting_room_card_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:waiting_room_app/wiating_room_cart.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  testWidgets('WaitingRoomCard displays the name correctly', (
+    WidgetTester tester,
+  ) async {
+    // Build our widget.
+    await tester.pumpWidget(
+      const MaterialApp(home: WaitingRoomCard(name: 'Alice')),
+    );
+    // Verify that the widget renders the correct name.
+    expect(find.text('Hello,'), findsOneWidget);
+    expect(find.text('Alice'), findsOneWidget);
+  });
+}
